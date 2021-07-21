01eb4337ddfc782ba4444effba3f337d0dfe5dae
FernandaAyres Silveira <fernandaayressilveirah@gmail.com>
2021-07-21 15:39:31 -0300

    aula 10

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
+// Incluindo na biblioteca o pacote 
 const http = require('http');
+const url = require('url');
+const queryString = require('query-string');
 
+
+// Define IP e porta (endereço url)
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+
+    //Pegar a pergunta na url    
+    const params= queryString.parse(url.parse(req.url,true).search);
+    console.log(params.pergunta);
+
+    //Verificar a pergunta e escolher uma resposta
+    let resposta;
+    if(params.pergunta =='melhor-filme'){
+      resposta = 'Star Wars';
+    }else if(params.pergunta =='melhor-tecnologia-backend'){
+      resposta ='Node JS';
+    }
+    else{
+      resposta ='não sei, desculpe :(';
+    }
+
+
+    //Retornar a resposta escolhida
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(resposta);
 });
 
+// Execução
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
